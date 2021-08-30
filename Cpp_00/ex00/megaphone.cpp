8a5c7e2004c487d454940581369f6bded0d5557f
oavelar42 <oavelar@student.42lisboa.com>
2021-08-30 19:45:28 +0100

    ex00 done , is easy to understand for me on the first time with c++

--- Cpp_00/ex00/megaphone.cpp
+++ Cpp_00/ex00/megaphone.cpp
@@ -6,14 +6,32 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 16:11:52 by oavelar           #+#    #+#             */
-/*   Updated: 2021/08/30 16:49:31 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/08/30 18:29:14 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main()
+int convert_lower(char c)
 {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    return (0);
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (int i = 1; i < ac ; i++)
+    {
+        for (int j = 0 ; av[i][j] ; j++)
+        {
+            if (convert_lower(av[i][j]) == 1)
+                av[i][j] -=  32;
+        }
+        std::cout << av[i];
+    }
+    std::cout << "\n";
     return (0);
 }
