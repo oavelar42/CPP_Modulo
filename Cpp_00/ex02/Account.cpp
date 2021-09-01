bf67852554d521c4bc4df34cb9642b5605a1e083
oavelar42 <oavelar@student.42lisboa.com>
2021-09-01 21:51:35 +0100

    I try ex02, but i cant

--- Cpp_00/ex02/Account.cpp
+++ Cpp_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 16:54:15 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/01 17:07:19 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/09/01 21:49:50 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,23 @@
 #include <ctime>
 #include <iostream>
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts( void ) {
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount( void ) {
+	return _totalAmount;
+}
+
+int Account::getNbDeposits( void ) {
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals( void ) {
+	return _totalNbWithdrawals;
+}
