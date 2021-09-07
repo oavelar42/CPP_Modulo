282332f39eaed728930264f4f3f3a79d1e214eda
oavelar42 <oavelar@student.42lisboa.com>
2021-09-07 17:38:08 +0100

    ex04 not correct , i dont know my mistake

--- Cpp_01/ex04/main.cpp
+++ Cpp_01/ex04/main.cpp
@@ -5,28 +5,58 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/05 21:37:01 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/06 19:19:42 by oavelar          ###   ########.fr       */
+/*   Created: 2021/09/07 16:09:27 by oavelar           #+#    #+#             */
+/*   Updated: 2021/09/07 17:33:57 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int ac, char **av)
 {
-    std::string filename;
-    std::string s1;
-    std::string s2;
-
-     
-    if (ac != 4)
+    if (ac == 4)
     {
-        std::cout << "Error: Arguments number incorrect" << std::endl;
-        return 1;
+        std::ifstream file(av[1]);
+        if (!file.is_open())
+            std::cout << "Operation file corrupted!" << std::endl;
+        if (file.peek() == EOF)  // use peek for check file is empty , if its empty turn error!
+            std::cout << "File is empty..." << std::endl;
+        std::string line;
+        std::string s1 = av[2];
+        std::string s2 = av[3];
+        if (s1.empty() || s2.empty())  // use empty() for chech string , if its len = 0 turn error!
+            std::cout << "s1 or s2 is empty." << std::endl;
+        std::string replace = ".replace";
+        std::string new_file = av[1] + replace;
+        std::ofstream replace_file(new_file);
+        if (!replace_file.is_open())
+            std::cout << "Failed to create new file!" << std::endl;
+        int count;
+        while (file)
+        {
+            getline(file, line);
+            count = 0;
+            while (1)
+            {
+                count = line.find(s1, count);
+                if (count == 0)
+                {
+                    replace_file << line;
+                    if (!file.eof())
+                        replace_file << "\n";
+                    break ;
+                }
+                else
+                {
+                    line = line.insert(count, s2).erase(count + s2.length(), s1.length());
+                    count += s2.length();
+                }
+            }
+        }
     }
-    filename = av[1];
-    s1 = av[2];
-    s2 = av[3];
-    
+    else
+        std::cout << "Wrong arguments!!!" << std::endl;
+    return (0);
 }
