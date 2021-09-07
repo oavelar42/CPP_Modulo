d4ac8da8785d0c43977e16fc05ec369d09111ec5
oavelar42 <oavelar@student.42lisboa.com>
2021-09-07 19:45:05 +0100

    ex04 good, i know my error... init ex05

--- Cpp_01/ex04/main.cpp
+++ Cpp_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 16:09:27 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/07 17:33:57 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/09/07 19:42:52 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,25 @@ int main(int ac, char **av)
     {
         std::ifstream file(av[1]);
         if (!file.is_open())
-            std::cout << "Operation file corrupted!" << std::endl;
-        if (file.peek() == EOF)  // use peek for check file is empty , if its empty turn error!
-            std::cout << "File is empty..." << std::endl;
+        {
+            std::cout << "Error: Could not open file." << std::endl;
+            return 1;
+        }    
+        if (file.peek() == EOF)                         // use peek for check file is empty , if its empty turn error!
+        {
+            std::cout << "Error: File is empty..." << std::endl;
+            return 2;
+        } 
         std::string line;
         std::string s1 = av[2];
         std::string s2 = av[3];
-        if (s1.empty() || s2.empty())  // use empty() for chech string , if its len = 0 turn error!
-            std::cout << "s1 or s2 is empty." << std::endl;
+        if (s1.empty() || s2.empty())                   // use empty() for chech string , if its len = 0 turn error!
+            std::cout << "Error: s1 or s2 is empty." << std::endl;  
         std::string replace = ".replace";
         std::string new_file = av[1] + replace;
         std::ofstream replace_file(new_file);
         if (!replace_file.is_open())
-            std::cout << "Failed to create new file!" << std::endl;
+            std::cout << "Error: Failed to create new file!" << std::endl;
         int count;
         while (file)
         {
@@ -41,7 +47,7 @@ int main(int ac, char **av)
             while (1)
             {
                 count = line.find(s1, count);
-                if (count == 0)
+                if (count == -1)
                 {
                     replace_file << line;
                     if (!file.eof())
@@ -57,6 +63,6 @@ int main(int ac, char **av)
         }
     }
     else
-        std::cout << "Wrong arguments!!!" << std::endl;
+        std::cout << "Error: Wrong arguments!!!" << std::endl;
     return (0);
 }
