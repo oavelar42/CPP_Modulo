c188c279889c840d6d42deea1314ab5f3e9b8a4b
oavelar42 <oavelar@student.42lisboa.com>
2021-09-10 22:10:34 +0100

    ex01 not correct

--- Cpp_02/ex01/Fixed.cpp
+++ Cpp_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:33:40 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/10 16:49:03 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/09/10 21:36:23 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,22 @@ Fixed::Fixed(void)
     _value = 0;
 }
 
-Fixed::Fixed(Fixed& n)
+Fixed::Fixed(const Fixed& n)
 {
     std::cout << "Copy constructor called" << std::endl;
     *this = n;
+}
+
+Fixed::Fixed(const int val)
+{
+    std::cout << "Int constructor called" << std::endl;
+    _value = val << _bits;
+}
+
+Fixed::Fixed(const float val)
+{
+    std::cout << "Float constructor called" << std::endl;
+    _value = roundf(val * (1 << _bits));
 }
 
 int Fixed::getRawBits(void) const
@@ -36,7 +48,7 @@ void Fixed::setRawBits( int const raw )
     _value = raw;
 }
 
-Fixed& Fixed::operator = (Fixed& n)
+Fixed& Fixed::operator = (const Fixed& n)
 {
     if (this == &n)
         return *this;
@@ -45,7 +57,25 @@ Fixed& Fixed::operator = (Fixed& n)
     return *this;
 }
 
+std::ostream& operator << (std::ostream& out, const Fixed& n)
+{
+    out << n.toFloat();
+    return out;
+}
+
 Fixed::~Fixed(void)
 {
     std::cout << LET "Destructor called" OFF << std::endl;
+}
+
+// converter 
+
+int Fixed::toInt(void) const
+{
+    return _value >> _bits;
+}
+
+float Fixed::toFloat(void) const
+{
+    return (float)_value / (float)(1 << _bits);
 }
--- Cpp_02/ex01/Fixed.hpp
+++ Cpp_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:33:34 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/10 17:00:06 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/09/10 20:30:56 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,26 @@ class Fixed
 {
     private:
         int _value;
-        const static int _bits = 8;
+        static const int _bits = 8;
 
     public:
         Fixed(void);
-        Fixed(Fixed& n);
+
+        Fixed(const Fixed& n);  //copiar
+        
+        Fixed(const int val);     // as duas para especificar os tipos
+        Fixed(const float val);
+
         ~Fixed(void);
-        Fixed& operator=(Fixed& n);
+        Fixed& operator=(const Fixed& n);
         int getRawBits( void ) const;
         void setRawBits( int const raw ); 
+
+        float toFloat(void) const;
+        int toInt(void) const;
     
 };
 
+std::ostream& operator << (std::ostream& out, const Fixed& n);
 
 #endif
