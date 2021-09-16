3f964efcb1358e73476cd64c5d6130b48df1be70
oavelar42 <oavelar@student.42lisboa.com>
2021-09-16 17:37:53 +0100

    understand how can i do this , already .HPP do something to can try init .CPP

--- Cpp_02/ex02/Fixed.hpp
+++ Cpp_02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:33:34 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/13 16:47:36 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/09/16 17:35:39 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,56 @@ class Fixed
         Fixed(const int val);     // as duas para especificar os tipos
         Fixed(const float val);
 
-        ~Fixed(void);
+        
         
         Fixed& operator=(const Fixed& n); //cpy assigment operator 
-        int getRawBits( void ) const;
-        void setRawBits( int const raw ); 
 
-        float toFloat(void) const;
-        int toInt(void) const;
+        /*
+               operador aritmético
+        */
+
+        Fixed				operator+(const Fixed &src) const;
+	    Fixed				operator-(const Fixed &src) const;
+	    Fixed				operator*(const Fixed &src) const;
+	    Fixed				operator/(const Fixed &src) const;
+
+        /*
+            Prefixo operador
+        */
+
+        Fixed&				operator++(void);
+	    Fixed&				operator--(void);
+
+         /*
+            '**' Prefixo operador
+        */
+
+	    Fixed				operator++(int tmp);
+	    Fixed				operator--(int tmp);
+
+         /*
+            operador de comparação
+        */
+
+        bool				operator>(const Fixed &src) const;
+	    bool				operator<(const Fixed &src) const;
+	    bool				operator>=(const Fixed &src) const;
+	    bool				operator<=(const Fixed &src) const;
+	    bool				operator==(const Fixed &src) const;
+	    bool				operator!=(const Fixed &src) const;
+
+        int getRawBits( void ) const;        //Retorna um valor de ponto fixo
+        void setRawBits( int const raw );    //Configurações de ponto fixo    
+
+        float toFloat(void) const;           //converte fixo para float
+        int toInt(void) const;               //convert fixo para inteiro   
+
+        static Fixed&		min(Fixed& a, Fixed& b);
+	    static const Fixed&	min(const Fixed& a, const Fixed& b);
+	    static Fixed&		max(Fixed& a, Fixed& b);
+	    static const Fixed&	max(const Fixed& a, const Fixed& b);    
+
+        ~Fixed(void);
     
 };
 
--- Cpp_02/ex02/main.cpp
+++ Cpp_02/ex02/main.cpp
@@ -6,27 +6,26 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:33:38 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/13 16:47:09 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/09/16 17:35:24 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main(void) {
+int main(void) 
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-Fixed a;
-Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-std::cout << a << std::endl;
-std::cout << ++a << std::endl;
-std::cout << a << std::endl;
-std::cout << a++ << std::endl;
-std::cout << a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
 
-std::cout << b << std::endl;
-
-std::cout << Fixed::max( a, b ) << std::endl;
-
-return 0;
+    return 0;
 }
 
 /*
