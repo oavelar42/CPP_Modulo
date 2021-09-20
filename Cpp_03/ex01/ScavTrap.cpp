d2947e1d6bf5c3e401ed3c72af4b66f347ff2af4
oavelar42 <oavelar@student.42lisboa.com>
2021-09-20 22:48:39 +0100

    Ex00 done , read about ex01 , its looks like ex00, lets start.....

--- Cpp_03/ex01/ScavTrap.cpp
+++ Cpp_03/ex01/ScavTrap.cpp
@@ -1,34 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/17 18:24:00 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/20 22:30:13 by oavelar          ###   ########.fr       */
+/*   Created: 2021/09/20 22:45:57 by oavelar           #+#    #+#             */
+/*   Updated: 2021/09/20 22:46:15 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
-int main(void)
-{
-	ClapTrap ota(OTA "Messi " OFF);
-	ClapTrap ota1(RED "Neymar " OFF);
-	ClapTrap ota2(GRE "Mbappé " OFF);
-
-	ota.attack("Mbappé ");
-	ota.takeDamage(2);
-    ota.beRepaired(1);
-
-	ota2.attack("Neymar ");
-	ota2.takeDamage(5);
-    ota2.beRepaired(4);
-
-	ota1.attack("Messi ");
-	ota1.takeDamage(3);
-	ota1.beRepaired(2);
-
-	return 0;
-}
--- Cpp_03/ex01/ScavTrap.hpp
+++ Cpp_03/ex01/ScavTrap.hpp
@@ -1,34 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/17 18:24:00 by oavelar           #+#    #+#             */
-/*   Updated: 2021/09/20 22:30:13 by oavelar          ###   ########.fr       */
+/*   Created: 2021/09/20 22:45:52 by oavelar           #+#    #+#             */
+/*   Updated: 2021/09/20 22:46:49 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
 
-int main(void)
-{
-	ClapTrap ota(OTA "Messi " OFF);
-	ClapTrap ota1(RED "Neymar " OFF);
-	ClapTrap ota2(GRE "Mbappé " OFF);
+# define OTA "\x1B[36m"
+# define RED "\x1B[31m"
+# define OFF "\033[0m"
+# define GRE "\x1B[32m"
 
-	ota.attack("Mbappé ");
-	ota.takeDamage(2);
-    ota.beRepaired(1);
+#include <iostream>
 
-	ota2.attack("Neymar ");
-	ota2.takeDamage(5);
-    ota2.beRepaired(4);
-
-	ota1.attack("Messi ");
-	ota1.takeDamage(3);
-	ota1.beRepaired(2);
-
-	return 0;
-}
+#endif
