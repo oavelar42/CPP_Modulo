0376e0e5f2fc357d64dfd327fe00cc9f4ec1c85c
oavelar42 <oavelar@student.42lisboa.com>
2021-10-04 17:48:37 +0100

    ex00 done , ex01 init looks the same the ex00 but more one class

---
+++ Cpp_05/ex01/Form.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/04 17:46:26 by oavelar           #+#    #+#             */
+/*   Updated: 2021/10/04 17:47:00 by oavelar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# define OTA "\x1B[36m"
+# define RED "\x1B[31m"
+# define OFF "\033[0m"
+# define LET "\033[1m"
+# define GRE "\x1B[32m"
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+    public:
+    
+
+
+    private :
+
+};
+
+
+#endif
