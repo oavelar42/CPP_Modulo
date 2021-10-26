ab0ec7a2e3a8428f7c6e33c902df00965761b3f2
oavelar42 <oavelar@student.42lisboa.com>
2021-10-26 18:26:44 +0100

    ex01 done, templates its good until now

--- Cpp_07/ex01/iter.hpp
+++ Cpp_07/ex01/iter.hpp
@@ -1,26 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 19:01:28 by oavelar           #+#    #+#             */
-/*   Updated: 2021/10/24 19:02:59 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/10/26 18:19:04 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+# define RED "\x1B[31m"
+# define GRE "\x1B[32m"
+# define OFF "\033[0m"
+
 #include <iostream>
+#include <string>
 
-class Iter
+template<class T>
+
+void print(T& str)
 {
-    public :
+    std::cout << str << std::endl;
+}
 
-    private :
+template<class T>
 
-};
+void iter(T *array, int len, void func(T const &))
+{
+    for(int i = 0; i < len; i++)
+        func(array[i]);
+}
+
 
 #endif
--- Cpp_07/ex01/main.cpp
+++ Cpp_07/ex01/main.cpp
@@ -6,8 +6,23 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 19:01:01 by oavelar           #+#    #+#             */
-/*   Updated: 2021/10/24 19:01:59 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/10/26 18:25:52 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+
+int main(void)
+{
+    int num[] = {'*', 2, 0, 2, 1};
+    std::cout << RED "Array Number :" OFF << std::endl;
+    iter(num, 5, print);
+
+    std::cout << "\n";
+
+    std::string str[4] = {"école", "42", "the", "best"};
+	std::cout << GRE "Array string :" OFF << std::endl;
+	iter(str, 4, print);
+    
+    return 0;
+}
