eb86a25c0b62eb38f2125536821d946b06f20f10
oavelar42 <oavelar@student.42lisboa.com>
2021-10-27 21:50:56 +0100

    ex02 done ,use 2 type of main for test...

--- Cpp_07/ex02/Array.hpp
+++ Cpp_07/ex02/Array.hpp
@@ -6,21 +6,81 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 19:06:57 by oavelar           #+#    #+#             */
-/*   Updated: 2021/10/24 19:07:30 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/10/27 21:49:22 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#define MAX_VAL 50
+
 #include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <exception>
+#include <time.h>
 
-class Array
+template<class T>
+
+class Array 
 {
-    public :
-
     private :
 
+        T *_array;
+        unsigned int _len;
+
+
+    public :
+
+        Array(void) { };
+        Array(const Array& copy);
+        Array(unsigned int val);
+        ~Array(void) { };
+        Array<T>& operator=(const Array<T> &copy);
+        T& operator [] (unsigned int count);
+        unsigned int size(void) const;      
 };
+
+template<class T>
+Array<T>::Array(unsigned int val) : _len(val)
+{
+    _array = new T[val];
+}
+
+template<class T>
+Array<T>::Array(const Array &copy) : _len(copy.size())
+{
+    _array = new T[_len];
+    for(unsigned int i = 0; i < _len; i++)
+        _array[i] = copy._array[i];
+}
+
+template<class T>
+Array<T>& Array<T>::operator=(const Array<T> &copy)
+{
+    _len = copy.size();
+    delete[] _array;
+    _array = new T[_len];
+    for(unsigned int i = 0; i < _len; i++)
+        _array[i] = copy._array[i];
+    return (*this);
+}
+
+template<class T>
+T& Array<T>::operator [] (unsigned int count)
+{
+    if (count >= _len)
+        throw std::exception();
+        //throw std::out_of_range("NOTHING");
+    return (_array[count]);    
+}
+
+template<class T>
+unsigned int Array<T>::size(void) const
+{
+    return (_len);
+}
+
 
 #endif
--- Cpp_07/ex02/main.cpp
+++ Cpp_07/ex02/main.cpp
@@ -6,18 +6,18 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 19:06:53 by oavelar           #+#    #+#             */
-/*   Updated: 2021/10/24 19:07:53 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/10/27 21:49:15 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <Array.hpp>
+#include "Array.hpp"
 
-#define MAX_VAL 750
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
+    
     for (int i = 0; i < MAX_VAL; i++)
     {
         const int value = rand();
@@ -59,6 +59,78 @@ int main(int, char**)
     {
         numbers[i] = rand();
     }
-    delete [] mirror;//
+
+    delete [] mirror;
+    
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+/*
+int         main(void)
+{
+    Array<int>	arr(4);
+
+    int * a = new int();
+
+    std::cout << "zero" << std::endl;
+    std::cout << *a << std::endl;
+
+	std::cout << std::endl;
+    try {
+	    for (unsigned int i = 0; i < arr.size(); ++i)
+		    std::cout << "1. " << i << " | " << arr[i] << std::endl;
+    }
+    catch (std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << "size: " << arr.size() << std::endl;
+	std::cout << std::endl;
+    
+    
+    std::cout << "int" << std::endl;
+	for (unsigned int i = 0; i < arr.size(); ++i)
+	{
+        arr[i] = 21 * (i % 4);
+		std::cout << "2. " << arr[i] << std::endl;
+	}
+	std::cout << "size: " << arr.size() << std::endl << std::endl;
+
+
+
+    std::cout << "float" << std::endl;
+    Array<float>    floats(7);
+    for (int i = 0; i < 7; ++i){
+        floats[i] = 1.0 / (i + 2);
+	    std::cout << floats[i] << std::endl;
+    }
+	std::cout << "size: " << floats.size() << std::endl << std::endl;
+
+    
+    Array<std::string> str(5);
+    str[0] = "42";
+    str[1] = " the";
+    str[2] = " best";
+    str[3] = " school";
+    str[4] = " in the world";
+    try {
+        str[5]= "test";
+        str[-4]= "test";
+    }
+    catch (const std::out_of_range& e){
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl << str[0] << str[1] << str[2] << str[3] << std::endl;
+	std::cout << "size: " << str.size() << std::endl << std::endl;
+
+    system("leaks Array | grep bytes");
+    return 0;
+}*/
