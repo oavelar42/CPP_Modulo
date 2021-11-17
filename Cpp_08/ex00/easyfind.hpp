27b343ed4c649233b9fd8655a1451d97f6f6f1bb
oavelar42 <oavelar@student.42lisboa.com>
2021-11-17 22:45:27 +0000

    back to cpp ,,,, vamomooooooooos

--- Cpp_08/ex00/easyfind.hpp
+++ Cpp_08/ex00/easyfind.hpp
@@ -6,12 +6,37 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 19:44:05 by oavelar           #+#    #+#             */
-/*   Updated: 2021/10/30 19:44:54 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/11/17 22:37:43 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# define RED "\x1B[31m"
+# define GRE "\x1B[32m"
+# define YEL "\x1B[33m"
+# define MAG "\x1B[35m"
+# define OFF "\033[0m"
+
+#include <iostream>
+#include <algorithm>
+#include <exception>
+#include <vector>
+#include <deque>
+#include <list>
+#include <iterator>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int val)
+{
+    typename T::iterator res;
+
+    res = std::find(container.begin(), container.end(), val);
+
+    if (*res != val)
+        throw std::exception();
+    return res;
+}
 
 #endif
--- Cpp_08/ex00/main.cpp
+++ Cpp_08/ex00/main.cpp
@@ -6,8 +6,36 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 19:43:15 by oavelar           #+#    #+#             */
-/*   Updated: 2021/10/30 19:44:01 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/11/17 22:41:19 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+
+int main(void)
+{
+    std::cout << RED "------Vector test------" OFF << std::endl; 
+    std::cout << "\n";
+    std::vector<int> teste;
+    teste.push_back(2);
+    teste.push_back(1);
+    teste.push_back(4);
+
+    try
+    {
+        std::vector<int>::const_iterator res = easyfind(teste, 1);
+        std::cout << *res << std::endl;
+
+        std::vector<int>::const_iterator res1 = easyfind(teste, 2);
+        std::cout << *res1 << std::endl;
+
+        std::vector<int>::const_iterator res2 = easyfind(teste, 3);
+        std::cout << *res2 << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Not found!" << std::endl;
+    }
+
+    return (0);
+}
